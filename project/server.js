813d9ebc4560a216b1ff93c4995a0bb1b8090161
Geo <geodornan@gmail.com>
2025-12-07 13:38:07 +0000

    API Key tinkering

---
+++ project/server.js
@@ -0,0 +1,2 @@
+const AMADEUS_TOKEN_URL  = "https://test.api.amadeus.com/v1/security/oauth2/token";
+const AMADEUS_HOTELS_URL = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city";
