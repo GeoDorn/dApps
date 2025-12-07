b4698251635d3d999aa7458094dd9e98a7d72496
Geo <geodornan@gmail.com>
2025-12-07 13:43:06 +0000

    InvokeAPI Working

--- project/server.js
+++ project/server.js
@@ -1,2 +1,3 @@
 const AMADEUS_TOKEN_URL  = "https://test.api.amadeus.com/v1/security/oauth2/token";
 const AMADEUS_HOTELS_URL = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city";
+
